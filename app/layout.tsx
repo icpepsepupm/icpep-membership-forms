8e7ef4b72b469137ea1439d49f7f99508ebd841e
CarlErosa <carlmelvinerosa3@gmail.com>
2026-08-28 12:36:16 +0800

    feat: change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,21 +21,8 @@ export const metadata: Metadata = {
     'Apply to join PUP ICPEP. Choose a department: Graphics, Marketing, Tech, Operations, or Secretariat.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/icpep-logo.jpg',
+    apple: '/icpep-logo.jpg',
   },
 }
 
